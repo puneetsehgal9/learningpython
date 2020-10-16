9dfb3197c2a6a6ceda32383f25fd24b6ec739d7b
Puneet <puneet@Puneets-MacBook-Air.local>
2020-10-16 10:07:34 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+print("whats super")
+print("got get stuff done")
+print(10+10)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print("Hello World")
+#
+# # This is an example of code comments
+# # Try printing 10 line
+# # Simply printing bladfasdf
+# print("badfsaf")
+# print("aslkdjfhajkdsf")
+# Maths
+# + Addition
+# - Substraction
+# / Division
+# * Multiplication
+# % Percent
+# < Less Than
+# > More Than
+# <= Less Than or equals to
+# >= More Than or equals to
+print(1+1)
+print(10-20)
+print("how are you today?")
+print(9894083989325+19493843984)
+print(100000/10)
+print(13*13)
+print(pow(10,10))
+print(pow(1.15,12))
+print(20%2)
+print(21%2)
+print(100*.1)
+print(27%3)
+print(100<=120)
+print(4820934802938409238>=9283409284092384)
+print(3458>=5849)
+print(45002/239034)
+print("akshat"+"puneet")
+print("akshat age is {}".format(24))
